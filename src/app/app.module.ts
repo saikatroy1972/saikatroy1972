ac03d8e86eadc40cf1457fe6e7e59dba3f7c20e4
saikatroy1972 <saikatroy1972@gmail.com>
2022-02-12 10:14:17 +0530

    Update on 12th Feb 2022

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LeftnavComponent } from './leftnav/leftnav.component';
+import { TopheadnavComponent } from './topheadnav/topheadnav.component';
+import { FootComponent } from './foot/foot.component';
+import { BodycontentComponent } from './bodycontent/bodycontent.component';
+import { PreloaderComponent } from './preloader/preloader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftnavComponent,
+    TopheadnavComponent,
+    FootComponent,
+    BodycontentComponent,
+    PreloaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/bodycontent/bodycontent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bodycontent',
+  templateUrl: './bodycontent.component.html',
+  styleUrls: ['./bodycontent.component.scss']
+})
+export class BodycontentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
